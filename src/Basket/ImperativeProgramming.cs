597b70afe156ec48ae6cf4b7e471c1d35b6a6342
Nadir BOUSLAH <n.bouslah@archimed.fr>
2018-05-31 12:24:18 +0200

    Nadir Aymen Bouslah B3 TP

---
+++ src/Basket/ImperativeProgramming.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket
+{
+    public class ImperativeProgramming
+    {
+        public static int CalculateBasketAmount(IList<BasketLineArticle>
+        basketLineArticles)
+        {
+            int i = 0;
+            return i;
+            // here your code implementation
+        }
+        public static ArticleDatabase GetArticleDatabase(string id)
+        {
+            // here your code implementation
+            return null;
+        }
+
+        public static ArticleDatabase GetArticleDatabaseMock(string id)
+        {
+            switch (id)
+            {
+                case "1":
+                    return new ArticleDatabase
+                    {
+                        Id = "1",
+                        Price = 1,
+                        Stock = 35,
+                        Label = "Banana",
+                        Category = "food"
+                    };
+                case "2":
+                    return new ArticleDatabase
+                    {
+                        Id = "2",
+                        Price = 500,
+                        Stock = 20,
+                        Label = "Fridge electrolux",
+                        Category = "electronic"
+                    };
+                case "3":
+                    return new ArticleDatabase
+                    {
+                        Id = "3",
+                        Price = 49,
+                        Stock = 68,
+                        Label = "Chair",
+                        Category = "desktop"
+                    };
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ src/BasketTest/BasketOperation_CalculateBasketAmountShould.cs
@@ -0,0 +1,89 @@
+﻿using Basket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BasketTest
+{
+    class BasketOperation_CalculateBasketAmountShould
+    {
+        private int amountTotal;
+        private bool articleDatabases;
+        private object basketLineArticle;
+
+        public class BasketTest
+        {
+            public List<BasketLineArticle> BasketLineArticles { get; set; }
+            public int ExpectedPrice { get; set; }
+        }
+        private static IEnumerable<Object[]> Baskets
+        {
+            get
+            {
+                return new[]
+                {
+                    new Object[]
+                    {
+                        new BasketTest(){ BasketLineArticles = new List<BasketLineArticle>
+                        {
+                            new BasketLineArticle{Id = "1" , Number = 12 , Label ="Banana"},
+                            new BasketLineArticle{Id = "2" , Number = 1 , Label ="Fridge electrolux"},
+                            new BasketLineArticle{Id = "3" , Number = 4 , Label ="Chair"},
+                        },
+                            ExpectedPrice =84868 }
+                    },
+                    new object[]
+                    {
+                        new BasketTest(){ BasketLineArticles = new List<BasketLineArticle>
+                        {
+                            new BasketLineArticle{Id = "1" , Number = 20 , Label ="Banana"},
+                            new BasketLineArticle{Id = "2" , Number = 6 , Label ="Chair"}
+                        },
+                        ExpectedPrice = 37520 }
+                    },
+                };
+            }
+        }
+        [TestMethod]
+        [DynamicData("Baskets")]
+        public void ReturnCorrectAmountGivenBasket(BasketTest basketTest)
+        {
+            var amoutTotal = 0;
+            foreach (var BasketLineArticle in basketTest.BasketLineArticles)
+            {
+                //retrive article from databas
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                var assemblyDirectory = Path.GetDirectoryName(path);
+                var jsonPath = Path.Combine(assemblyDirectory, "articledatabase.json");
+                IList<ArticleDatabase> articleDatabases =
+                JsonConvert.DeserializeObject<List<ArticleDatabase>>(File.ReadAllText(jsonPath));
+                var article = articleDatabases.First(articleDatabase =>
+                articleDatabase.Id == BasketLineArticle.Id);
+                // Calculate amount
+                var amount = 0;
+                switch (article.Category)
+                {
+                    case "food":
+                        amount += article.Price * 100 + article.Price * 12;
+                        break;
+                    case "electronic":
+                        amount += article.Price * 100 + article.Price * 20 + 4;
+                        break;
+                    case "desktop":
+                        amount += article.Price * 100 + article.Price * 20;
+                        break;
+                }
+                amountTotal += amount * BasketLineArticle.Number;
+            }
+            Assert.AreEqual(amountTotal, basketTest.ExpectedPrice);
+        }
+    }
+}
+
